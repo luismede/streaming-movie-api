26ae5e5b366a6c9c83e7720a9fbc8756b0a79d29
Luis Henrique <luis.medeiros@germinare.org.br>
2025-11-09 00:08:23 -0300

    feat: implementar swagger para documentação

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('API de Filmes')
+    .setDescription('API de Filmes')
+    .setVersion('1.0')
+    .addTag('filmes')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config); 
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
